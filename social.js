f7811d5911a057984d047c7f0115d6958e8078d1
StationedInTheField <ask@stationedinthefield.com>
2020-10-24 07:52:30 -0700

    upd

--- social.js
+++ social.js
@@ -69,5 +69,81 @@ export class QuestSocial {
 
 
 
+  async getSocialProfileForChannelPubKey(channel,chPubKey){
+
+      let folderBase = this.channel.getParticipantFolders(channel);
+      if(folderBase.length > 0){
+        for(let i=0;i<folderBase.length;i++){
+          for(let i2=0;i2<folderBase[i]['participants'].length;i2++){
+
+            if(folderBase[i]['participants'][i2]['pubKey'] == chPubKey && typeof folderBase[i]['participants'][i2]['nick'] != 'undefined' &&  folderBase[i]['participants'][i2]['nick'] != ""){
+
+
+              let p;
+              try{
+                p = await this.profile.getByChannelPubKey(folderBase[i]['participants'][i2]['pubKey']);
+              }catch(e){console.log(e)}
+              if(typeof p != 'undefined' && typeof p['alias'] != 'undefined'){
+                p['nick'] = folderBase[i]['participants'][i2]['nick'];
+                return p;
+              }
+
+              return { nick: folderBase[i]['participants'][i2]['nick'] }
+            }
+            else if(folderBase[i]['participants'][i2]['pubKey'] == chPubKey){
+                let p;
+                try{
+                  p = await this.profile.getByChannelPubKey(folderBase[i]['participants'][i2]['pubKey']);
+                }catch(e){console.log(e)}
+                if(typeof p != 'undefined' && typeof p['alias'] != 'undefined'){
+                  return p;
+                }
+            }
+          }
+        }
+      }
+
+      let p;
+      try{
+        p = await this.profile.getByChannelPubKey(chPubKey);
+      }catch(e){console.log(e)}
+
+      if(typeof p != 'undefined' && typeof p['alias'] != 'undefined'){
+        return p;
+      }
+
+       return { alias: 'Anonymous' };
+
+
+    }
+
+
+
+ async getMentionItems(channel){
+   let results = [];
+   // get channel pubkeys of participants
+   let fullParticipantList = this.dolphin.getChannelParticipantList(channel)['cList'].split(',');
+   if(typeof fullParticipantList['cList'] == 'undefined'){
+     return [];
+   }
+
+   let participantCListArray = this.q.os.channel.getParticipantList(channel);
+   // get social pubkeys of participants
+   for(let chPubKey of participantCListArray){
+     let p = this.channel.getSocialProfileForChannelPubKey(channel,chPubKey);
+     if(typeof p['nick'] != 'undefined' && p['nick'].length > 0){
+       results.push('<'+p['nick']+'|'+p['key']['pubKey']+'>');
+     }
+     else if(typeof p['alias'] != 'undefined' && p['alias'].length > 0 && p['alias'] != 'Anonymous'){
+       results.push('<'+p['alias']+'|'+p['key']['pubKey']+'>');
+     }
+   }
+
+   // get display names of participants
+   return results;
+ }
+
+
+
 
 }
